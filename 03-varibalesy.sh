7915e77256b15db689fc16b7fbb203ddc027e50d
visu90522 <visu90522@gmail.com>
2024-01-16 16:57:07 +0530

    first6

---
+++ 03-varibalesy.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+echo "Hello world Devops"
+
+PERSON1=$1  # first value from command line 
+PERSON2=$2  # second value from the command line 
+
+echo "$PERSON1: Hi $PERSON2"
+echo "Hi $PERSON1"
