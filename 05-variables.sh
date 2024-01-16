9b1f9c2004039fa122596ca6bbedf2664f97e6bc
visu90522 <visu90522@gmail.com>
2024-01-16 17:14:24 +0530

    first8

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,3 +5,12 @@ echo " please enter the user name"
 read USERNAME # user need to enter
 
 echo "Username entered is: $USERNAME"
+
+
+
+echo " please enter the upassword"
+
+read PASSWORD # user need to enter
+
+echo "password entered is: $PASSWORD"
+
