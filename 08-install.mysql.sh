78f0c2c1ed83393d52c24f0d04db4d76c09fd26f
visu90522 <visu90522@gmail.com>
2024-01-17 14:36:53 +0530

    changes

--- 08-install.mysql.sh
+++ 08-install.mysql.sh
@@ -9,3 +9,24 @@ then
 else
   echo "you are not the root user"
 fi
+
+# it is our responsibility again to check installation is success or not
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of mysql is error"
+    exit 1
+else
+    echo "Installation of mysql is success"
+fi
+
+yum install postfix -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of postfix is error"
+    exit 1
+else
+    echo "Installation of postfix is success"
+fi
