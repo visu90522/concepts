dc05685d643e069fe5527eee897f433a1941b5c8
visu90522 <visu90522@gmail.com>
2024-01-17 16:44:01 +0530

    changes

--- 08-install.mysql.sh
+++ 08-install.mysql.sh
@@ -6,27 +6,18 @@ if [ $USER -ne 0 ]
 then
   echo "ERROR:: please run with root user"
   exit1
-else
-  echo "you are not the root user"
+# else
+#   echo "you are the root user"
 fi
 
 # it is our responsibility again to check installation is success or not
 yum install mysql -y
 
-if [ $? -ne 0 ]
-then
-    echo "Installation of mysql is error"
-    exit 1
-else
-    echo "Installation of mysql is success"
-fi
+if [ $? -ne 0]
 
-yum install postfix -y
-
-if [ $? -ne 0 ]
 then
-    echo "Installation of postfix is error"
-    exit 1
+    echo "Mysql is error"
+    exit1
 else
-    echo "Installation of postfix is success"
+    echo "Installlation is success"
 fi
