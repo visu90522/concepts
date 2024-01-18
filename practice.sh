bb8a6d338e180689905561abedc675aa782ca2a1
visu90522 <visu90522@gmail.com>
2024-01-18 18:52:18 +0530

    new changes

--- practice.sh
+++ practice.sh
@@ -1,3 +1,15 @@
+
+VALIDATE() {
+    if [ $? -ne 0 ]
+    then
+        echo "Installation of mysql is failure"
+        exit1
+    else
+        echi "Installation of mysql is success"
+    fi
+}
+
+
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
@@ -5,3 +17,8 @@ then
     echo "ERROR:: please run with root user"
     exit1
 fi
+
+yum install mysql -y
+
+VALIDATE $?
+
