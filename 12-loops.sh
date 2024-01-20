81ced82e5ea7b050ea4bb9628a2d78a5d39c4456
visu90522 <visu90522@gmail.com>
2024-01-20 18:02:17 +0530

    new commit

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"                                            
+N="\e[0m"
+
+if [ $USERID -ne 0 ]
+then    
+    echo -e "$R ERROR:: run with the root user $N"
+    exit1
+fi
+
+
 #all arguments
 for i in $@
 do
     yum install $i -y
 done
+
+# improvements
+# implement log files
+# implement colors
+# implement validations
+# implement validations through functions
+# your script should check package is already installed or not, if already installed print yellow color
+# if installed just print package is alredy installed, it should not run install command
